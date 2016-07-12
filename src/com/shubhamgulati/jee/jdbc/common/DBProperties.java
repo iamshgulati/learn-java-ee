1aa8a94dc838647e987a33de651d1835751e43fb
Shubham Gulati <shubhamgulati91@gmail.com>
2016-07-12 16:53:22 +0530

    added assignments

--- src/com/shubhamgulati/jee/jdbc/common/DBProperties.java
+++ src/com/shubhamgulati/jee/jdbc/common/DBProperties.java
@@ -1,13 +1,13 @@
 package com.shubhamgulati.jee.jdbc.common;
 
-public class DBProperties {
+public interface DBProperties {
 	
 	// for Windows
-	public static final String DB_PROPERTIES_FILE_PATH = "D:\\Work\\GitHub\\learn-java-ee\\src\\com\\shubhamgulati\\jee\\jdbc\\common\\DBProperties.properties";
+	String DB_PROPERTIES_FILE_PATH = "D:\\Work\\GitHub\\learn-java-ee\\src\\com\\shubhamgulati\\jee\\jdbc\\common\\DBProperties.properties";
 	
 	// for Linux
-	// public static final String FILE_PATH = "/home/shubham/Work/GitHub/learn-java-ee/src/com/shubhamgulati/jdbc/common/DBProperties.properties";
+	// String DB_PROPERTIES_FILE_PATH = "/home/shubham/Work/GitHub/learn-java-ee/src/com/shubhamgulati/jdbc/common/DBProperties.properties";
 	
-	public static final String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
-	public static final String DB_CONNECTION_URL = "jdbc:mysql://127.0.0.1:3306/bece6_db";
+	String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+	String DB_CONNECTION_URL = "jdbc:mysql://127.0.0.1:3306/bece6_db";
 }
